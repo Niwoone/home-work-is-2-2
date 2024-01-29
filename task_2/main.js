e31a7ad2a0c48c83f34aaa1772bd70762e0eb96d
Niwoone <gashchenko62@mail.ru>
2024-01-29 15:20:42 +0300

    Написали программу на конструкции if else какую нужно одевать в определенную температуру

---
+++ task_2/main.js
@@ -0,0 +1,9 @@
+let temperature = prompt('Введите температуру')
+if (temperature > 30) {
+    console.log('Вы можете одеть легкую одежду')
+} else if (temperature >= 20){
+    console.log('Вы можете одеть теплую одежду')
+}else if (temperature < 20){
+    console.log('Сегодня холодно надейвайте теплую одежду')
+}
+    
